df698e37d3429df05d228fc985b453a2f0dbd07f
Lctrs <jerome@prmntr.me>
2022-01-07 15:35:02 +0100

    feat(deps): switch to roave/better-reflection

--- config/services.php
+++ config/services.php
@@ -6,10 +6,8 @@ use Lctrs\PhpExtStubsGenerator\Application\Node\GetNodes;
 use Lctrs\PhpExtStubsGenerator\Infrastructure\Cli\GenerateStubsForExtCommand;
 use Lctrs\PhpExtStubsGenerator\Infrastructure\Node\GetNodesGroupedByNamespace;
 use Lctrs\PhpExtStubsGenerator\Infrastructure\Node\GetNodesUsingBetterReflection;
-use PHPStan\BetterReflection\BetterReflection;
-use PHPStan\BetterReflection\Reflector\ClassReflector;
-use PHPStan\BetterReflection\Reflector\ConstantReflector;
-use PHPStan\BetterReflection\Reflector\FunctionReflector;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflector\DefaultReflector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
@@ -19,18 +17,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(BetterReflection::class);
 
-    $services->set(ClassReflector::class)
-        ->factory([service(BetterReflection::class), 'classReflector']);
-    $services->set(FunctionReflector::class)
-        ->factory([service(BetterReflection::class), 'functionReflector']);
-    $services->set(ConstantReflector::class)
-        ->factory([service(BetterReflection::class), 'constantReflector']);
+    $services->set(DefaultReflector::class)
+        ->factory([service(BetterReflection::class), 'reflector']);
+    $services->alias(Reflector::class, DefaultReflector::class);
 
     $services->set(GetNodesUsingBetterReflection::class)
         ->args([
-            service(ClassReflector::class),
-            service(FunctionReflector::class),
-            service(ConstantReflector::class),
+            service(Reflector::class),
         ]);
     $services->set(GetNodesGroupedByNamespace::class)
         ->args([
--- src/Infrastructure/Node/GetNodesUsingBetterReflection.php
+++ src/Infrastructure/Node/GetNodesUsingBetterReflection.php
@@ -11,10 +11,8 @@ use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\Const_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
-use PHPStan\BetterReflection\Reflector\ClassReflector;
-use PHPStan\BetterReflection\Reflector\ConstantReflector;
-use PHPStan\BetterReflection\Reflector\FunctionReflector;
 use ReflectionExtension;
+use Roave\BetterReflection\Reflector\Reflector;
 
 use function array_keys;
 use function array_map;
@@ -22,18 +20,11 @@ use function assert;
 
 final class GetNodesUsingBetterReflection implements GetNodes
 {
-    private ClassReflector $classReflector;
-    private FunctionReflector $functionReflector;
-    private ConstantReflector $constantReflector;
+    private Reflector $reflector;
 
-    public function __construct(
-        ClassReflector $classReflector,
-        FunctionReflector $functionReflector,
-        ConstantReflector $constantReflector
-    ) {
-        $this->classReflector    = $classReflector;
-        $this->functionReflector = $functionReflector;
-        $this->constantReflector = $constantReflector;
+    public function __construct(Reflector $reflector)
+    {
+        $this->reflector = $reflector;
     }
 
     public function forExtension(string $name): Nodes
@@ -44,12 +35,12 @@ final class GetNodesUsingBetterReflection implements GetNodes
             array_map(
                 fn (
                     string $className
-                ): ClassLike => $this->classReflector->reflect($className)->getAst(),
+                ): ClassLike => $this->reflector->reflectClass($className)->getAst(),
                 array_keys($extension->getClasses())
             ),
             array_map(
                 function (string $functionName): Function_ {
-                    $ast = $this->functionReflector->reflect($functionName)->getAst();
+                    $ast = $this->reflector->reflectFunction($functionName)->getAst();
 
                     assert($ast instanceof Function_);
 
@@ -59,7 +50,7 @@ final class GetNodesUsingBetterReflection implements GetNodes
             ),
             array_map(
                 function (string $constantName): Const_|Expression {
-                    $ast = $this->constantReflector->reflect($constantName)->getAst();
+                    $ast = $this->reflector->reflectConstant($constantName)->getAst();
 
                     if ($ast instanceof Stmt) {
                         return $ast;
